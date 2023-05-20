7adf1073adf2e58f6a1851fba9d8891784d5dbc7
garret-1123 <73963164+garret-1123@users.noreply.github.com>
2023-05-20 09:11:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() {
+song = loadSound("music.mp3")
+song2 = loadSound("active1.mp3")
+
+}
+function setup() {
+    canvas = createCanvas(600,500)
+canvas.center()
+video = createCapture(VIDEO)
+video.hide
+}
+function draw() {
+image(video,0,0,600,500)
+
+
+}
+
+function play() {
+song.play()
+
+
+}
